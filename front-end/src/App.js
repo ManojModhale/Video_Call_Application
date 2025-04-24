09a7a12b8d79f95f972afd8a83a1228a73a96379
ManojModhale <shreemodhale0102@gmail.com>
2025-04-24 14:01:55 +0530

    Re-add front-end directory with React app files

---
+++ front-end/src/App.js
@@ -0,0 +1,16 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <>
+      <div>
+        <h1>Video Call Application</h1>
+        <p>Welcome to the MERN Stack Project!</p>
+      </div>
+    </>
+
+  );
+}
+
+export default App;
